d92072b5dde91bb63605f83a9b6dacbd63a786c8
Lena Höding <l.hoeding@tu-braunschweig.de>
2023-05-11 12:58:25 +0200

    first plant

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     plugins: [...getMapsOptimizers(maps)],
     server: {
         host: "localhost",
+        port: 8080,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
