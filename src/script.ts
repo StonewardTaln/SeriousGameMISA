c2e77dc904af74bb552707f252b7838147d6e153
Lena Höding <l.hoeding@tu-braunschweig.de>
2023-06-20 16:06:53 +0200

    local variable typeof

--- src/script.ts
+++ src/script.ts
@@ -3,28 +3,29 @@
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 //tests, examples
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+/*WA.chat.sendChatMessage('Hello world', 'Mr Robot');
 
 WA.chat.onChatMessage((message => {
   console.log('The user typed a message:', message);
 }));
+*/
 
 //ts-file
 let currentPopup: any = undefined;
+
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
   console.log('Scripting API ready');
   console.log('Player tags: ',WA.player.tags)
 
-  WA.room.area.onEnter('clocky').subscribe(() => {
-      console.log('player entered zone');
-      const today = new Date();
-      const time = today.getHours() + ":" + today.getMinutes();
-      currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-  })
+  WA.player.state.foo = 0;
+  console.log('virable foo: ', WA.player.state.foo);
 
-  WA.room.area.onLeave('clocky').subscribe(closePopup)
+  uhrUhr();
+
+  runRounds();
+  
 
   // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
   bootstrapExtra().then(() => {
@@ -38,4 +39,30 @@ function closePopup(){
       currentPopup.close();
       currentPopup = undefined;
   }
+}
+
+function runRounds() {
+  //todo
+}
+
+function uhrUhr() {
+  console.log('test123');
+  //Uhr-Uhr
+  WA.room.area.onEnter('clocky').subscribe(() => {
+    console.log('player entered zone ');
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+
+    //with const or var we check what type foo is and can work with it
+    var myVariable = WA.player.state.foo;
+    if (typeof myVariable == "number") { //or "string"
+      WA.player.state.foo = myVariable + 1;
+      console.log(WA.player.state.foo + " times");
+    } else {
+      console.log("Error: WA.player.state.foo is not a number.");
+    }
+})
+
+WA.room.area.onLeave('clocky').subscribe(closePopup)
 }
